14f0acb74efaec0ca66b97de077ca994565598c3
Matías Pierobon <matias.pierobonm@gmail.com>
2018-11-21 20:20:27 -0300

    create express server and serve

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const api = require('./api');
+
+const port = process.env.PORT || 3000
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(morgan('tiny'));
+
+api(app);
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json(err);
+  throw err;
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
