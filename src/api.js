8d952b4c51178ae9175cc49310e1e4d646ef4097
Pedro Brost <pedro.brost97@gmail.com>
2018-11-29 00:39:16 -0300

    Add resolve endpoint

--- src/api.js
+++ src/api.js
@@ -1,4 +1,6 @@
 const bonita = require('./bonita');
+const status = require('http-status');
+const error = require('http-errors');
 
 const handle = fn => (req, res, next) => fn(req, res, next).catch(next);
 
@@ -6,10 +8,11 @@ const resolvers = {};
 
 const forCase = caseId =>
   new Promise((resolve, reject) => {
-    resolvers[caseId] = resolve;
+    resolvers[caseId] = { resolve, reject };
   });
 
-const resolve = caseId => resolvers[caseId];
+const resolve = caseId => resolvers[caseId].resolve;
+const reject = caseId => resolvers[caseId].reject;
 
 module.exports = app => {
   app.get(
@@ -17,9 +20,28 @@ module.exports = app => {
     handle(async (req, res) => {
       const credentials = await bonita.login();
       const processId = await bonita.getProcess(credentials, 'pool3');
-      const variables = [{ name: "token", value: req.headers.authorization }]
+      const variables = [{ name: 'token', value: req.headers.authorization }];
       const caseId = await bonita.initiate(credentials, processId, variables);
-      console.log("[BONITA] Started case id", caseId)
+      console.log('[BONITA] Started case id', caseId);
+
+      const list = await forCase(caseId);
+
+      res.json(list);
+    })
+  );
+
+  app.get(
+    '/prepare-sale',
+    handle(async (req, res) => {
+      const credentials = await bonita.login();
+      const processId = await bonita.getProcess(credentials, 'prepare-sale');
+      const variables = [
+        { name: 'token', value: req.headers.authorization },
+        { name: 'productsCart', value: req.body.productsCart },
+        { name: 'couponCode', value: req.body.couponCode || '' }
+      ];
+      const caseId = await bonita.initiate(credentials, processId, variables);
+      console.log('[BONITA] Started case id', caseId);
 
       const list = await forCase(caseId);
 
@@ -31,6 +53,16 @@ module.exports = app => {
     '/resolve',
     handle(async (req, res) => {
       resolve(req.headers.caseid)(req.body);
+      res.status(status.OK).json({ message: 'Case finished' });
+    })
+  );
+
+  app.put(
+    '/reject',
+    handle(async (req, res) => {
+      reject(req.headers.caseid)(
+        error(req.body.status || 500, req.body.message || 'Internal error')
+      );
     })
   );
 };
